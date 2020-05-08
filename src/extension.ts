e66e05b908890f7bebc25c81f0e425dad65ab6b3
Andre Weinand <aweinand@microsoft.com>
2020-05-08 16:21:19 +0200

    add a dynamic debug config

--- src/extension.ts
+++ src/extension.ts
@@ -13,6 +13,18 @@ export function activate(context: vscode.ExtensionContext) {
 	const projectContainer = new ProjectContainer();
 
 	context.subscriptions.push(vscode.notebook.registerNotebookProvider('nodebook', new NodebookProvider(projectContainer)));
+	context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('node', {
+		provideDebugConfigurations(folder: vscode.WorkspaceFolder) {
+			return [
+				{
+					name: 'Debug Nodebook',
+					type: 'node',
+					request: 'attach',
+					port: 12345
+				}
+			];
+		}
+	}, vscode.DebugConfigurationProviderTriggerKind.Dynamic));
 }
 
 export function deactivate() {
